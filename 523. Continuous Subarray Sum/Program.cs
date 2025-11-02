9c6e261b30193bba9c12daa39506fbce6d93670d
Mahmoud Kassem <modykassem123@gmail.com>
2025-11-02 22:58:33 +0200

    Add project files.

---
+++ 523. Continuous Subarray Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(CheckSubarraySum([2, 4, 3], 6));
+
+bool CheckSubarraySum(int[] nums, int k)
+{
+    int n = nums.Length;
+    int prefix = 0;
+
+    Dictionary<int, int> map = new() { { 0, -1 } };
+    for (int i = 0; i < n; i++)
+    {
+        prefix += nums[i];
+
+        int remainder = k == 0 ? prefix : prefix % k;
+
+        if (map.ContainsKey(remainder))
+        {
+            if (i - map[remainder] > 1)
+                return true;
+        }
+        else
+            map.Add(remainder, i);
+    }
+
+    return false;
+}
