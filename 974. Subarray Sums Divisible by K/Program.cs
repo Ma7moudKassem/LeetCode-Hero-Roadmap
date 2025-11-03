40d681af69d5b9f8fd0f582b7dd4f8fa9681635e
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-11-03 11:13:00 +0200

    Runtime 7 ms Beats 73.17% | Memory 52.10 MB Beats 58.54%

---
+++ 974. Subarray Sums Divisible by K/Program.cs
@@ -0,0 +1,27 @@
+﻿int SubarraysDivByK(int[] nums, int k)
+{
+    int n = nums.Length;
+
+    int count = 0;  
+    int prefix = 0;
+
+    Dictionary<int, int> map = new() { { 0, -1 } };
+
+    for (int i = 0; i < n; i++)
+    {
+        prefix += nums[i];
+
+        int remainder = k == 0 ? prefix : prefix % k;
+        if (remainder < 0)
+            remainder += k;
+
+        if (map.ContainsKey(remainder))
+        {
+            count += map[remainder];
+            map[remainder]++;
+        }
+        else map[remainder] = 1;
+    }
+
+    return count;
+}
