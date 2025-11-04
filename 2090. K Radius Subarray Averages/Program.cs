6c4597dd0786e10711d87b3b49605488763b5b4d
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-11-04 13:09:01 +0200

    Runtime 6 ms Beats 37.80% | Memory 80.70 MB Beats 48.82%

---
+++ 2090. K Radius Subarray Averages/Program.cs
@@ -0,0 +1,35 @@
+﻿var r = GetAverages([7, 4, 3, 9, 1, 8, 5, 2, 6], 3);
+
+int[] GetAverages(int[] nums, int k)
+{
+    if(k == 0)
+        return nums;
+
+    int n = nums.Length;
+    int[] result = new int[n];
+    Array.Fill(result, -1);
+
+    int windowSize = 2 * k + 1;
+    if(n < windowSize)
+        return result;
+
+    long[] prefix = new long[nums.Length];
+    prefix[0] = nums[0];
+
+    for (int i = 1; i < nums.Length; i++)
+        prefix[i] = nums[i] + prefix[i - 1];
+
+    for (int i = k; i < n - k; i++)
+    {
+        int right = i + k;
+        int left = i - k - 1;
+
+        long total = prefix[right];
+        if(left >= 0)
+            total -= prefix[left];
+
+        result[i] = (int)(total / windowSize);
+    }
+
+    return result;
+}
