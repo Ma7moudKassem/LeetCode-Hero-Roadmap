25503f00924fb26558b0179a1ce79aa1703bdb8b
Mahmoud Kassem <modykassem123@gmail.com>
2025-11-07 02:54:22 +0200

    Runtime 1 ms Beats 100.00% | Memory 55.97 MB Beats 78.77%

---
+++ 1423. Maximum Points You Can Obtain from Cards/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(MaxScore([96, 90, 41, 82, 39, 74, 64, 50, 30], 8));
+
+int MaxScore(int[] cardPoints, int k)
+{
+    if (k == 0)
+        return 0;
+
+    int n = cardPoints.Length;
+
+    int window = 0;
+    for (int i = 0; i < k; i++)
+        window += cardPoints[i];
+
+    int result = window;
+
+    //Sliding window
+    for (int i = 0; i < k; i++)
+    {
+        window -= cardPoints[k - i - 1];
+        window += cardPoints[n - i - 1];
+        
+        result = Math.Max(result, window);
+    }
+
+    return result;
+}
