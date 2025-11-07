5ab80a1fc0cbffe93493b18d72770ca5789f6afa
Mahmoud Kassem <modykassem123@gmail.com>
2025-11-07 04:07:15 +0200

    Runtime 1 ms Beats 100.00% | Memory 66.77 MB Beats 67.34%

---
+++ 238. Product of Array Except Self/Program.cs
@@ -0,0 +1,22 @@
+﻿var arr = ProductExceptSelf([1, 2, 3, 4]);
+
+int[] ProductExceptSelf(int[] nums)
+{
+    int n = nums.Length;
+
+    int[] prefixArray = new int[n];
+    int[] suffixArray = new int[n];
+
+    prefixArray[0] = 1;
+    for (int i = 1; i < n; i++)
+        prefixArray[i] = prefixArray[i - 1] * nums[i - 1];
+
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        prefixArray[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return prefixArray;
+}
