2bef1e78d6b34c6bb19f779fa61c04161a674555
Mahmoud Kassem <modykassem123@gmail.com>
2025-11-07 17:59:09 +0200

    Runtime 10 ms Beats 100.00% | Memory 117.36 MB Beats 97.65%

---
+++ 380. Insert Delete GetRandom O(1)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+public class RandomizedSet
+{
+    private List<int> _store;
+    private Dictionary<int, int> _map;
+    private Random _random;
+    public RandomizedSet()
+    {
+        _store = [];
+        _map = [];
+        _random = new Random();
+    }
+
+    public bool Insert(int val)
+    {
+        if (_map.ContainsKey(val))
+            return false;
+
+        _map.Add(val, _store.Count);
+        _store.Add(val);
+
+        return true;
+    }
+
+    public bool Remove(int val)
+    {
+        if (!_map.ContainsKey(val))
+            return false;
+
+        int index = _map[val];
+        int last = _store[^1];
+
+        _store[index] = last;
+        _store.RemoveAt(_store.Count - 1);
+        
+        _map[last] = index;
+        _map.Remove(val);
+        
+        return true;
+    }
+
+    public int GetRandom()
+    {
+        if(_store.Count == 0)
+            return 0;
+
+        int rondomeIdx = _random.Next(_store.Count - 1);
+        return _store[rondomeIdx];
+    }
+}
